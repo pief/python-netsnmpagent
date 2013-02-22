792977b5657e200a13794257bd12622b8d127021
Pieter Hollants <pieter@hollants.com>
2013-02-22 12:15:51 +0100

    Work around unresolved dependencies bug in net-snmp 5.4.x libraries

--- netsnmpapi.py
+++ netsnmpapi.py
@@ -16,6 +16,9 @@ UINT_MAX = 4294967295
 # Make libnetsnmpagent available via Python's ctypes module. We do this globally
 # so we can define C function prototypes
 try:
+	# Workaround for net-snmp 5.4.x that has a bug with unresolved dependencies
+	# in its libraries (http://sf.net/p/net-snmp/bugs/2107)
+	libnsh = ctypes.cdll.LoadLibrary(ctypes.util.find_library("netsnmphelpers"))
 	libnsa = ctypes.cdll.LoadLibrary(ctypes.util.find_library("netsnmpagent"))
 except:
 	raise Exception("Could not load libnetsnmpagent! Is net-snmp installed?")
